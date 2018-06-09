96d0906efd2969da5a25d004743ee06070884f12
William Johnson dos Santos Okano <williamokano@gmail.com>
2018-06-09 16:04:55 -0300

    Fixed GlobalExceptionHandler to return 404

--- commons/src/main/java/br/udi/william/demos/distributedtracing/commons/commons/GlobalExceptionHandler.java
+++ commons/src/main/java/br/udi/william/demos/distributedtracing/commons/commons/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package br.udi.william.demos.distributedtracing.commons.commons;
 
 import br.udi.william.demos.distributedtracing.commons.commons.exception.NotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ResourceValue handleNotFoundException(NotFoundException notFoundException) {
         return notFoundException.getResourceValue();
