c91b415d562ab002d266257501c331e0fd9ab55e
William Johnson dos Santos Okano <williamokano@gmail.com>
2018-06-10 04:16:24 -0300

    Added clientTracing

--- order-web/src/main/kotlin/br/udi/william/demos/distributedtracing/orderweb/config/FeignClientsConfig.kt
+++ order-web/src/main/kotlin/br/udi/william/demos/distributedtracing/orderweb/config/FeignClientsConfig.kt
@@ -2,10 +2,14 @@ package br.udi.william.demos.distributedtracing.orderweb.config
 
 import br.udi.william.demos.distributedtracing.commons.commons.catalog.CatalogAPI
 import br.udi.william.demos.distributedtracing.commons.commons.customer.CustomerAPI
+import feign.Client
 import feign.Feign
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.okhttp.OkHttpClient
+import feign.opentracing.TracingClient
+import io.opentracing.Tracer
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.netflix.feign.support.SpringMvcContract
 import org.springframework.context.annotation.Bean
@@ -14,13 +18,21 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 open class FeignClientsConfig {
 
-    private val client: OkHttpClient by lazy { OkHttpClient() }
+    @Autowired
+    private lateinit var client: Client
+
+    @Autowired
+    private lateinit var tracer: Tracer
 
     @Value("\${customer.url}")
     private lateinit var customerUrl: String
 
     @Value("\${catalog.url}")
     private lateinit var catalogUrl: String
+
+    @Bean
+    open fun feignClient(): Client =
+        OkHttpClient()
 
     @Bean
     open fun customerApi(): CustomerAPI = client(customerUrl)
@@ -30,7 +42,7 @@ open class FeignClientsConfig {
 
     private inline fun <reified T> client(url: String) =
         Feign.builder()
-            .client(client)
+            .client(TracingClient(client, tracer))
             .contract(SpringMvcContract())
             .encoder(JacksonEncoder())
             .decoder(JacksonDecoder())
--- order-web/src/main/kotlin/br/udi/william/demos/distributedtracing/orderweb/config/JaegerOpentracingConfig.kt
+++ order-web/src/main/kotlin/br/udi/william/demos/distributedtracing/orderweb/config/JaegerOpentracingConfig.kt
@@ -3,11 +3,13 @@ package br.udi.william.demos.distributedtracing.orderweb.config
 import io.jaegertracing.Configuration
 import io.jaegertracing.samplers.ProbabilisticSampler
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 
 @org.springframework.context.annotation.Configuration
 open class JaegerOpentracingConfig {
 
     @Bean
+    @Primary
     open fun jaegerTracer(): io.opentracing.Tracer {
         val sampler = Configuration.SamplerConfiguration()
         sampler.withType(ProbabilisticSampler.TYPE).withParam(1)
